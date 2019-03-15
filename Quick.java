0122935e3b23bf7b188ceda8eeb15b4ea9caac36
moodyRahman <mrahman01@stuy.edu>
2019-03-14 21:35:39 -0400

    swap

--- Quick.java
+++ Quick.java
@@ -2,11 +2,19 @@ import java.util.Random;
 
 public class Quick{
 
+public static void swap(int[] data, int idxa, int idxb){
+	int temp = data[idxa];
+	data[idxa] = data[idxb];
+	data[idxb] = temp;
+}
+
+
 public static void partition(int[] data, int start, int end){
 	Random randomGenerator = new Random();
 	int randomIndex = randomGenerator.nextInt(end - start + 1) + start;
 
-		System.out.println(randomIndex);
+	System.out.println(randomIndex);
+
 
 }
 
